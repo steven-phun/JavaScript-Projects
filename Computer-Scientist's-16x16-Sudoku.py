599d7543b8444aa7e8d18f3f783c29e7d75ec3b9
Steven Phun <phun.steven@gmail.com>
2020-05-05 10:01:24 -0700

    added function terminology in header comment

--- Computer-Scientist's-16x16-Sudoku.py
+++ Computer-Scientist's-16x16-Sudoku.py
@@ -1,1 +1,12 @@
+##
 # created by Steven Phun on May 5, 2020.
+#
+#
+# this GUI python program   allows the user to play or have the program solve a 16x16 Sudoku.
+#                           the game is based on the classic 9x9 Sudoku where the basic rules is the same.
+#                           place the numbers 0-9 and letters A-F into each row, column and 4x4 row once.
+#
+# -sudoku                   is a partially completed grid.
+# -grid                     has 16 rows, 16 columns, and 16 boxes, each having 16 squares (256 total).
+# -constraint               is that each element(numbers/letters) appears only once in each row, column, and row.
+# -pencil                   write a number or letter in the square.
