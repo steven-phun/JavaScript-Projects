70e155df28640e87ba115b4de0a58a323e5a996b
Steven Phun <phun.steven@gmail.com>
2020-05-14 21:17:53 -0700

    took out fixed gridsize and adjusted the height and width of each indivdual cells.

--- 16x16-Sudoku-Solver/main.js
+++ 16x16-Sudoku-Solver/main.js
@@ -5,15 +5,20 @@ function drawGrid() {
   const table = document.createElement("table");
   table.className = "sudoku-grid";
 
-  for (i = 0; i < 16; i++) {
-    const row = table.insertRow();
-    row.className = "sudoku-row";
-    for (j = 0; j < 16; j++) {
-      const col = row.insertCell();
-      col.className = "sudoku-col";
+  for (i = 0; i < 5; i++) {
+    const tbody = document.createElement("tbody");
+    tbody.className = "sudoku-tbody";
+    table.append(tbody);
+    for (i = 0; i < 16; i++) {
+      const row = tbody.insertRow();
+      row.className = "sudoku-row";
+      for (j = 0; j < 16; j++) {
+        const col = row.insertCell();
+        col.className = "sudoku-col";
+      }
     }
+    getTag.append(table);
   }
-  getTag.append(table);
 }
 
 drawGrid();
