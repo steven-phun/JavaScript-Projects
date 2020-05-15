c210fb124674e6fb89b6cc1528ad6ed840e95397
Steven Phun <phun.steven@gmail.com>
2020-05-14 23:12:22 -0700

    added the 4x4 section border ouline.

--- 16x16-Sudoku-Solver/main.js
+++ 16x16-Sudoku-Solver/main.js
@@ -5,20 +5,21 @@ function drawGrid() {
   const table = document.createElement("table");
   table.className = "sudoku-grid";
 
-  for (i = 0; i < 5; i++) {
-    const tbody = document.createElement("tbody");
-    tbody.className = "sudoku-tbody";
-    table.append(tbody);
-    for (i = 0; i < 16; i++) {
-      const row = tbody.insertRow();
-      row.className = "sudoku-row";
-      for (j = 0; j < 16; j++) {
-        const col = row.insertCell();
-        col.className = "sudoku-col";
+  for (i = 0; i < 16; i++) {
+    const row = table.insertRow();
+    row.classList.add("sudoku-row");
+    if (i % 4 === 0 && i !== 0) {
+      row.classList.add("sudoku-row-section");
+    }
+    for (j = 0; j < 16; j++) {
+      const col = row.insertCell();
+      col.classList.add("sudoku-col");
+      if (j % 4 === 0 && j !== 0) {
+        col.classList.add("sudoku-col-section");
       }
     }
-    getTag.append(table);
   }
+  getTag.append(table);
 }
 
 drawGrid();
