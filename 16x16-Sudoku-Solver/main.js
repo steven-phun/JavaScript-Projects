13d343641ba8d149d1fff7ef465dabb2c7ffb259
Steven Phun <phun.steven@gmail.com>
2020-05-14 23:33:35 -0700

    fixed border to 3px to make it visiable when desktop zooms out.

--- 16x16-Sudoku-Solver/main.js
+++ 16x16-Sudoku-Solver/main.js
@@ -8,13 +8,13 @@ function drawGrid() {
   for (i = 0; i < 16; i++) {
     const row = table.insertRow();
     row.classList.add("sudoku-row");
-    if (i % 4 === 0 && i !== 0) {
+    if (i !== 0 && i % 4 === 0) {
       row.classList.add("sudoku-row-section");
     }
     for (j = 0; j < 16; j++) {
       const col = row.insertCell();
       col.classList.add("sudoku-col");
-      if (j % 4 === 0 && j !== 0) {
+      if (j !== 0 && j % 4 === 0) {
         col.classList.add("sudoku-col-section");
       }
     }
