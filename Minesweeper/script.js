28fbf69d8b44932e0fd909f771a95c507c6b565c
Steven Phun <phun.steven@gmail.com>
2020-06-07 23:31:39 -0700

    added starter codes for Minesweeper

--- Minesweeper/script.js
+++ Minesweeper/script.js
@@ -1,6 +1,14 @@
 /**
- * @author Steven Phun on June 8, 2020
+ * This JavaScript program allows the user to play the classic game of Minesweeper.
  *
- *
- * this JavaScript program allows the user to play the classic game of Minesweeper
+ * @author Steven Phun
+ * @since June 8, 2020.
  */
+
+
+/*jshint esversion: 6 */
+
+
+/*** JavaScript Classes ***/
+
+/*** JavaScript Functions ***/
