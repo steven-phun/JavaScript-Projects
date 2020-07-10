01ac8b9ef1dbfde5013badc9171630e7c3bab4e4
Steven Phun <phun.steven@gmail.com>
2020-07-09 20:36:26 -0700

    added jshint and @author and @since.

--- College-Schedule-Maker/script.js
+++ College-Schedule-Maker/script.js
@@ -2,4 +2,10 @@
  * This JavaScript program allows the user to create a weekly schedule for school.
  *
  * The schedule can be printed, saved onto the computer, or exported to allow changes in the future.
+ *
+ *
+ * @author Steven Phun
+ * @since July 9, 2020.
  */
+
+/*jshint esversion: 6 */
