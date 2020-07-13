55ee6eb8a875a3ff7d36491081fab44dbdf7ec59
Steven Phun <phun.steven@gmail.com>
2020-07-12 18:12:44 -0700

    added class Time to convert hours and minute to standard time format HH:MM

--- Course-Schedule-Maker/script.js
+++ Course-Schedule-Maker/script.js
@@ -26,10 +26,10 @@ class Schedule {
         /** CSS color class instances */
 
         /** class instances. */
-        this.course = [];  // {array}  represents a collection of all the courses in the schedule.
-        this.weekSize = 7; // {number} represents how many days in a week will be displayed.
-        this.earliest = 9; // {number} represents earliest time the schedule will display.
-        this.latest = 14;  // {number} represents the latest time the schedule will display.
+        this.course = [];                  // {array}  represents a collection of all the courses in the schedule.
+        this.weekSize = 7;                 // {number} represents how many days in a week will be displayed.
+        this.earliest = new Time(9); // {number} represents earliest time the schedule will display.
+        this.latest = new Time(14);  // {number} represents the latest time the schedule will display.
 
         this.setup();
     }
@@ -54,7 +54,7 @@ class Schedule {
         let count = 1;
 
         for (let time = this.earliest; time <= this.latest; time++) {
-            this.table.rows[count].cells[0].innerHTML = time;
+            this.table.rows[count].cells[0].innerHTML = time.timeToString();
             count++;
         }
     }
@@ -77,8 +77,45 @@ class Course {
         this.start = null; // {number} represents when the course starts.
         this.end = null;   // {number} represents when the course ends.
     }
-
 }
+
+
+/**
+ * @class represents time in hours:minute format.
+ *
+ * @param hour   {number} represent the amount of hours.
+ * @param minute {number} represents the amount of minutes.
+ */
+class Time {
+    constructor(hour, minute=5) {
+        this.hour = hour;                    // {number} represent the amount of hours.
+        this.minute = this.toMinute(minute); // {string} represents the amount of minutes.
+    }
+
+    /**
+     * @function convert a single digit minute to standard time format.
+     *           --> 1 minute = HH:01.
+     *           --> 5 minutes = HH:05.
+     * @param minute {number} the minute to convert.
+     *
+     * @return a string that represents the minute.
+     */
+    toMinute(minute) {
+        if (minute >= 10) return minute.toString();
+
+        return `0${minute}`;
+    }
+
+    /**
+     * @function converts given hour and minute in a 12 hour HH:MM format.
+     */
+    timeToString() {
+        if (this.hour > 12) return `${this.hour - 12}:${this.minute} pm`
+
+        return `${this.hour}:${this.minute} am`;
+    }
+}
+
 
 /*** JavaScript Functions ***/
 const schedule = new Schedule();
