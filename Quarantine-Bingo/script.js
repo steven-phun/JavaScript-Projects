4c1f1d2c105083e669b2f483b7345a9d13e14280
Steven Phun <phun.steven@gmail.com>
2020-07-17 23:12:23 -0700

    user is now able to see the scorecard.

--- Quarantine-Bingo/script.js
+++ Quarantine-Bingo/script.js
@@ -11,9 +11,60 @@
 /*** JavaScript Classes ***/
 
 
+/**
+ * @class represents the bingo scorecard.
+ */
+class Bingo {
+    constructor() {
+        /** HTML tag instances*/
+        this.scorecard = document.querySelector("#scorecard>table");
+
+
+        /** CSS class/id instances */
+
+        /** class instances. */
+        this.size = 5; // {number} represents the width and length of the scorecard.
+
+        this.buildScorecard();
+    }
+
+    /**
+     * @function generate a bingo scorecard.
+     */
+    buildScorecard() {
+        for (let i = 0; i < this.size; i++) {
+            const row = this.scorecard.insertRow(); // insert <tr>.
+            for (let j = 0; j < this.size; j++) {
+                row.insertCell(); // insert <td>.
+            }
+        }
+    }
+}
+
+
+/**
+ * @class represents one individual cell on the scorecard.
+ */
+class Square {
+
+}
+
+
+/**
+ * @class represents different themes to initialize the bingo board.
+ */
+class Theme {
+    constructor() {
+        this.theme = null;
+        this.collection = []; // {array} a collection of theme with questions.
+    }
+}
+
+
 
 /*** JavaScript Functions ***/
 
 
 
 // global instance
+const bingo = new Bingo;
