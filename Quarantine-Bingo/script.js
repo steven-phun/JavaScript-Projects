7f457417a21366d21140f59c9c55ceefc1de99f2
Steven Phun <phun.steven@gmail.com>
2020-07-18 00:13:39 -0700

    added class Quarantine.

--- Quarantine-Bingo/script.js
+++ Quarantine-Bingo/script.js
@@ -15,15 +15,18 @@
  * @class represents the bingo scorecard.
  */
 class Bingo {
-    constructor() {
+    /**
+     * @param theme {Object} set the theme for this round.
+     */
+    constructor(theme=new Quarantine()) {
         /** HTML tag instances*/
         this.scorecard = document.querySelector("#scorecard>table");
-
 
         /** CSS class/id instances */
         this.selected = "selected-cell";
 
         /** class instances. */
+        this.theme = theme; // {array} a collection of questions to initialize the game with.
         this.row = null; // {number} the row index of selected cell.
         this.col = null; // {number} the column index of selected cell.
         this.size = 5; // {number} represents the width and length of the scorecard.
@@ -100,12 +103,48 @@ class Square {
 
 
 /**
- * @class represents different themes to initialize the bingo board.
+ * @class represents a quarantine theme to initialize the bingo board.
  */
-class Theme {
+class Quarantine {
     constructor() {
         this.theme = null;
-        this.collection = []; // {array} a collection of theme with questions.
+        this.questions = this.getQuestions(); // {array} a collection of theme with questions.
+        this.center = "FREE SPACE (wore a mask)"; // {string} represents the center square of the scoreboard.
+    }
+
+    /**
+     * @function fill the array with questions.
+     *
+     * @return {array} of questions.
+     */
+    getQuestions() {
+        const q1 = "slept in pas noon";
+        const q2 = "baked for fun";
+        const q3 = "watched more than 3 episodes of a show in on day";
+        const q4 = "took a walk outside to exercise";
+        const q5 = "video called wearing sweat, shorts, or pajama bottoms";
+        const q6 = "started a workout routine or health regime";
+        const q7 = "had to cancel a planned celebration or trip";
+        const q8 = "made an unnecessary online purchase";
+        const q9 = "started a puzzle";
+        const q10 = "video called to hangout with friends";
+        const q11 = "purchased hand sanitizer or hand soap";
+        const q12 = "did not leave home property for more than 5 days in a row";
+        const q13 = "cleaned or organized something at home";
+        const q14 = "made a tik-tok video or participated in one";
+        const q15 = "forgot to unmute yourself in a video call";
+        const q16 = "got an at home or DIY haircut";
+        const q17 = "joined the Facebook group 'Zoom Memes for Self-Quaranteens";
+        const q18 = "disconnected from a Zoom call because of bad connectivity";
+        const q19 = "picked up a new hobby";
+        const q20 = "started a new book";
+        const q21 = "did not know what Zoom was before March 2020";
+        const q22 = "had food delivered to your house";
+        const q23 = "went to sleep past 2am";
+        const q24 = "asked 'what day is it?'";
+
+        return [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10, q11, q12, q13,
+                q14, q15, q16, q17, q18, q19, q20, q21, q22, q23, q24];
     }
 }
 
