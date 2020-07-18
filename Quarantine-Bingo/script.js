33321508da3ff9737196983a7ff4840b397bbec0
Steven Phun <phun.steven@gmail.com>
2020-07-17 22:16:05 -0700

    added starter codes.

---
+++ Quarantine-Bingo/script.js
@@ -0,0 +1,19 @@
+/**
+ * This JavaScript program allows the user to play the game Bingo with Quarantine as its theme.
+ *
+ * @author Steven Phun
+ * @since July 17, 2020.
+ */
+
+/*jshint esversion: 6 */
+
+
+/*** JavaScript Classes ***/
+
+
+
+/*** JavaScript Functions ***/
+
+
+
+// global instance
