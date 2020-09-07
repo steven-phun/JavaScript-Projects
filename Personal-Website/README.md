3fa58421bc093b286325a36b42d3aec3a09f83a7
Steven Phun <phun.steven@gmail.com>
2020-09-07 10:58:42 -0700

    initial commit.

---
+++ Personal-Website/README.md
@@ -0,0 +1,2 @@
+# Personal-Website
+A website developed in HTML, CSS, and JavaScript to find my personal projects, about me, and more
---
+++ Personal-Website/script.js
@@ -0,0 +1,12 @@
+
+/**
+ * This JavaScript program allows the user to play the game Bingo with Quarantine as its theme.
+ *
+ * @author Steven Phun
+ * @since September 4, 2020.
+ */
+
+/*jshint esversion: 6 */
+
+
+/*** JavaScript Functions ***/
